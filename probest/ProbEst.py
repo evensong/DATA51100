1ac9d2f5cb8019d6e529537f93f41e3615eab42f
evensong <phantomdragonrising@gmail.com>
2021-09-25 19:17:24 -0600

    Major overhaul of code. code focuses on reading all data for each car make into its own column.

--- probest/ProbEst.py
+++ probest/ProbEst.py
@@ -4,31 +4,59 @@
 # Fall 2021
 # Programming Assignment 4: ProbEst
 
+from pandas import DataFrame
 import pandas as pd
 
 
 def main():
-    intro = 'DATA51100, FALL 2021\nNAME: Ben Haws\nPROGRAMMING ASSIGNMENT#4'
+    intro = 'DATA51100, FALL 2021\nNAME: Ben Haws\nPROGRAMMING ASSIGNMENT#4\n'
     print(intro)
 
     # import cars data as DataFrame & select make & aspiration columns
     car_data = pd.read_csv('cars.csv')[['make', 'aspiration']]
 
-    # create a series listing total cars of each make
-    totals = car_data.value_counts(subset=['make'], sort=False)
+    # create empty data frame in the correct shape to fill
+    car_stats = DataFrame()
+    car_stats.index = set(car_data['make'])
+    car_stats.index.name = 'make'
 
-    # create a series listing total cars of each aspiration for each make
-    type_totals = car_data.value_counts(subset=['make', 'aspiration'], sort=False)  # FIXME: need to show rows with 0 of a type
+    # create series of total models for each make and join to main df
+    total = car_data.value_counts(subset='make', sort=False)
+    total.name = 'total'
+    total = DataFrame(total)
+    car_stats = pd.merge(car_stats, total, left_index=True, right_index=True)
 
-    # join these series into DataFrame, add column for probability
-    type_totals.name = 'type totals'  # add name to allow DataFrame operations
-    totals.name = 'totals'
+    # create a series of total standard models for each make and merge
+    std = car_data[car_data['aspiration'] == 'std'].value_counts(sort=False)
+    std = std.to_frame()
 
-    totals = type_totals.to_frame().join(totals)
+    # cleans up the dataframe, leaving a one column frame ready to merge
+    std.reset_index(inplace=True)
+    std.drop('aspiration', axis=1, inplace=True)
+    std.rename(columns={0: 'std'}, inplace=True)
+    std.set_index('make', inplace=True)
 
-    totals['prob'] = totals['type totals'] / totals['totals'] * 100
-    print(totals)  # FOR TESTING ONLY!! DELETE ME!!!
+    # merge into car_stats--outer merge keeps makes with no std models
+    car_stats = pd.merge(car_stats, std, how='outer', left_index=True, right_index=True)
+    car_stats.fillna(0, inplace=True)
 
+    # create a series of total turbo models for each make and merge
+    turbo = car_data[car_data['aspiration'] == 'turbo'].value_counts(sort=False)
+    turbo = turbo.to_frame()
+
+    # cleans up the dataframe, leaving a one column frame ready to merge
+    turbo.reset_index(inplace=True)
+    turbo.drop('aspiration', axis=1, inplace=True)
+    turbo.rename(columns={0: 'turbo'}, inplace=True)
+    turbo.set_index('make', inplace=True)
+
+    # merge into car_stats--outer merge keeps makes with no turbo models
+    car_stats = pd.merge(car_stats, turbo, how='outer', left_index=True, right_index=True)
+    car_stats.fillna(0, inplace=True)
+
+
+    car_stats.sort_index(inplace=True)
+    print(car_stats)
     return
 
 
